60b44efe34f431939387ba3d9b8b9be829908943
MilkyGee <milkcreame@gmail.com>
2023-05-01 22:11:55 -0700

    weapons update 8/10

--- main.py
+++ main.py
@@ -8,41 +8,55 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT),pygame.RESIZABLE)
         self.title = pygame.display.set_caption("Twisted Empress")
-        self.icon = pygame.display.set_icon(pygame.image.load("Assets\icon.png"))
+        self.icon = pygame.display.set_icon(pygame.image.load("Assets/icon.png"))
         self.clock = pygame.time.Clock()
-        self.title_font = pygame.font.Font('Assets/royal-intonation.ttf',48)
-        self.font = pygame.font.Font('Assets/royal-intonation.ttf',32)
+        self.title_font = pygame.font.Font('Assets/Font/royal-intonation.ttf',48)
+        self.font = pygame.font.Font('Assets/Font/royal-intonation.ttf',32)
         self.running = True
-        self.background_spritesheet = Spritesheet("Assets\map2.png")
-        self.character_spritesheet = Spritesheet("Assets\player2.png")
-        self.intro_background = pygame.image.load("Assets\map2.png")
-        self.intro_background.set_alpha(25)
         
+        self.background_spritesheet = Spritesheet("Assets/map.png")
+        self.character_spritesheet = Spritesheet("Assets/Entities/player.png")
+        self.attack_spritesheet = Spritesheet("Assets/Objects/attacks.png")
+        self.weapon_spritesheet = Spritesheet("Assets/Objects/weapons.png")
+        #self.health_spritesheet = Spritesheet("Assets\UI\hp.png")
+        
+        self.intro_background = pygame.image.load("Assets/map.png").convert()
+        self.intro_background.set_alpha(100)
+
         self.level = 1
         self.area = 1
+
     def new(self):
         self.playing = True
 
         self.all_sprites = pygame.sprite.LayeredUpdates()
-        self.blocks = pygame.sprite.LayeredUpdates() #Stores all wall sprites
+        self.walls = pygame.sprite.LayeredUpdates() #Stores all wall sprites
         self.enemies = pygame.sprite.LayeredUpdates() #Stores all enemy sprites
         self.attacks = pygame.sprite.LayeredUpdates() #Stores all attack hitbox sprites
 
-        self.background = Background(self,0,0) #0 plains | 1 desert | 2 forest | 3 castle
-        self.player = Player(self, 1, 2, 0)
-    
+        self.background = Background(self,self.level-1,self.area-1) #0 plains | 1 desert | 2 forest | 3 castle
+        self.player = Player(self, 7, 7,2)
+        self.arrows = []
     def events(self):
         #game loop events
+
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    arrow = PlayerArrow(self, self.player.x+32,self.player.y, mouse_pos)
+                    self.arrows.append(arrow) 
     def update(self):
         self.all_sprites.update()
-
+        
     def draw(self):
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
+
+        self.screen.blit(self.player.weapon_copy,(self.player.x+32,self.player.y))
         self.clock.tick(FPS)
         pygame.display.update()
     
@@ -56,7 +70,32 @@ class Game:
     
     def game_over(self):
         pass
-    
+    def fade(self,width, height): 
+        fade = pygame.Surface((width, height))
+        fade.fill((0,0,0))
+        for alpha in range(0, 300):
+            fade.set_alpha(alpha)
+            self.playing = False
+            self.screen.blit(fade, (0,0))
+            pygame.display.update()
+            pygame.time.delay(1)
+            self.playing = True
+    def next_level(self):
+        if self.level == 5:
+            if self.area < 4:
+                self.area += 1
+            else:
+                self.area = 1
+        
+        if self.level < 5:
+            self.level += 1
+        else:
+            self.level = 1
+        self.background.kill()
+        for i in self.arrows:
+            i.kill()
+        self.background = Background(self,self.level-1,self.area-1)
+        self.fade(WIN_WIDTH,WIN_HEIGHT)
     def intro_screen(self):
         intro = True
 
@@ -79,9 +118,9 @@ class Game:
             if play_button.is_pressed(mouse_pos, mouse_pressed):
                 intro = False
             if play_button.is_hovered(mouse_pos, mouse_pressed):
-                play_button.content = "hi"
+                play_button.image.set_alpha(255)
             else:
-                play_button.content = "no"
+                play_button.image.set_alpha(255/8)
             self.screen.blit(self.intro_background, (0,0))
             self.screen.blit(title, title_rect)
             self.screen.blit(version, version_rect)
--- sprites.py
+++ sprites.py
@@ -10,6 +10,7 @@ class Spritesheet:
         sprite.blit(self.sheet, (0,0), (x,y,width,height))
         sprite.set_colorkey(BLACK)
         return sprite
+
 class Background(pygame.sprite.Sprite):
     def __init__(self,game, x, y):
         self.game = game
@@ -21,14 +22,18 @@ class Background(pygame.sprite.Sprite):
         self.width = WIN_WIDTH
         self.height = WIN_HEIGHT
         self.image = self.game.background_spritesheet.get_sprite(x*self.width,y*self.width,self.width,self.height)
-        self.image.set_alpha(75)
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, game, x, y, power = 0):
         self.game = game
         self.power = power
+
+        self.health = 4
+        self.max_health = 4
+
         self._layer = PLAYER_LAYER #Bottom BG, Enemies, Attacks, UI
         self.groups = self.game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
@@ -38,7 +43,7 @@ class Player(pygame.sprite.Sprite):
         self.height = TILESIZE
 
         self.image = self.game.character_spritesheet.get_sprite(1,1,self.width,self.height)
-
+        self.weapon = self.game.weapon_spritesheet.get_sprite(self.power*48,0,self.width,self.height)
         self.x_change = 0 #x_vel
         self.y_change = 0 #y_vel
 
@@ -58,11 +63,12 @@ class Player(pygame.sprite.Sprite):
         self.y += self.y_change
         self.x_change = 0
         self.y_change = 0
-
-        print(self.y)
-        if self.y < 5:
+        
+        if self.y <= 0:
             self.rect.y = WIN_HEIGHT - 32
             self.y = self.rect.y
+            self.game.next_level()
+
     def movement(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -104,8 +110,13 @@ class Player(pygame.sprite.Sprite):
                     self.facing = "up_left"
                 else:
                     self.facing = "up_right"
+    def handle_weapon(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
 
+        angle = (180 / pi) * -atan2(mouse_y-self.y, mouse_x-self.x)
+        self.weapon_copy = pygame.transform.rotate(self.weapon,angle)
     def animate(self):
+        self.handle_weapon()
         down = self.game.character_spritesheet.get_sprite(0,self.power*48, self.width, self.height)
         right = self.game.character_spritesheet.get_sprite(48,(self.power)*48, self.width, self.height)
         left = pygame.transform.flip(right, True, False)
@@ -119,10 +130,73 @@ class Player(pygame.sprite.Sprite):
         up_left = pygame.transform.flip(up_right, True, False)
         
         self.image = locals()[self.facing]
+class PlayerArrow(pygame.sprite.Sprite):
+    def __init__(self,game,x,y,mouse_pos) -> None:
+        self.x = x
+        self.y = y
+        self.width = TILESIZE
+        self.height = TILESIZE
+        self.game = game
 
+        self._layer = PLAYER_LAYER #Bottom BG, Enemies, Attacks, UI
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.speed = 7
+        self.angle = atan2(y-mouse_y,x-mouse_x)
+        self.x_vel = cos(self.angle) * self.speed
+        self.y_vel = sin(self.angle) * self.speed
+        
+        self.image = self.game.attack_spritesheet.get_sprite(48*self.game.player.power,0,self.width,self.height)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        
+
+        angle = (180 / pi) * -atan2(mouse_y-self.y, mouse_x-self.x)-90
+        self.arrow_copy = pygame.transform.rotate(self.image,angle)
+        self.image = self.arrow_copy
+    def update(self):
+        self.x -= self.x_vel
+        self.y -= self.y_vel
+        self.rect.x = self.x
+        self.rect.y = self.y
+        if self.x > WIN_WIDTH:
+            self.kill()
+        if self.y > WIN_HEIGHT:
+            self.kill()
+        pygame.time.delay(0)
+
+       
+
+##############################################################
+
+class Text:
+    def __init__(self, x, y, width, height, fg, bg, content, fontsize) -> None:
+        self.font = pygame.font.Font('Assets/Font/royal-intonation.ttf', fontsize)
+
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+        self.content = content
+        self.fg = fg
+        self.bg = bg
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(self.bg)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.x
+        self.rect.y = self. y
+        self.text = self.font.render(self.content, True, self.fg, self.bg)
+        self.text_rect = self.text.get_rect(center=(self.width/2, self.height/2))
+        self.image.blit(self.text, self.text_rect)
 class Button: 
     def __init__(self, x, y, width, height, fg, bg, content, fontsize) -> None:
-        self.font = pygame.font.Font('Assets/royal-intonation.ttf', fontsize)
+        self.font = pygame.font.Font('Assets/Font/royal-intonation.ttf', fontsize)
 
         self.x = x
         self.y = y
